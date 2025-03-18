daabd92510f1b74aad9fc61b1b2c8d50ea82b20f
Rait Rips <rait.rips@tptlive.ee>
2025-03-18 10:56:13 +0200

    Add search to pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,25 +1,25 @@
 <script setup>
 import { computed } from 'vue';
 
-let {info, current} = defineProps(['info', 'current']);
+let { info, current } = defineProps(['info', 'current']);
 
 let pageNumbers = computed(() => {
     let pages = [];
-    for(let i = 1; i<=3; i++){
+    for (let i = 1; i <= 3 && i <= info.pages; i++) {
         pages[i] = i;
     }
-    if(current > 2) {
+    if (current > 2) {
         pages.push('...');
     }
-    if(current > 2 && current < info.pages-1) {
-        for(let i = current-1; i<=current+1; i++){
+    if (current > 2 && current < info.pages - 1) {
+        for (let i = current - 1; i <= current + 1; i++) {
             pages[i] = i;
         }
     }
-    if(current < info.pages-2) {
+    if (current < info.pages - 2) {
         pages.push('...');
     }
-        for(let i = info.pages-2; i <= info.pages; i++) {
+    for (let i = info.pages - 2; i <= info.pages; i++) {
         pages[i] = i;
     }
     return pages.filter(p => p);
@@ -34,17 +34,17 @@ console.log(pageNumbers.value)
         <ul class="pagination-list">
             <li v-for="page in pageNumbers">
                 <span class="pagination-ellipsis" v-if="page === '...'">&hellip;</span>
-                <a class="pagination-link is-current" :aria-label="`Page ${page}`" aria-current="page" v-else-if="page === current">{{ page }}</a>
-                <a href="#" class="pagination-link " :aria-label="`Go to page ${page}`" v-else>{{ page }}</a>
+
+                <a class="pagination-link is-current"
+                    :aria-label="`Page ${page}`"
+                    aria-current="page"
+                    v-else-if="page === current">{{ page }}</a>
+
+                <a @click="$emit('page', page)"
+                    class="pagination-link "
+                    :aria-label="`Go to page ${page}`"
+                    v-else>{{ page }}</a>
             </li>
-            <!-- <li><span class="pagination-ellipsis">&hellip;</span></li>
-            <li><a href="#" class="pagination-link" aria-label="Goto page 45">45</a></li>
-            <li>
-                <a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a>
-            </li>
-            <li><a href="#" class="pagination-link" aria-label="Goto page 47">47</a></li>
-            <li><span class="pagination-ellipsis">&hellip;</span></li>
-            <li><a href="#" class="pagination-link" aria-label="Goto page 86">86</a></li> -->
         </ul>
     </nav>
 </template>
--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -7,31 +7,68 @@ import Pagination from '../components/Pagination.vue';
 const characters = ref([]);
 const info = ref([]);
 const currentPage = ref(1);
+const searchValue = ref('');
+const error = ref('');
 await getCharacters('https://rickandmortyapi.com/api/character');
 
-async function getCharacters(url) {
-    let response = await axios.get(url);
-    console.log(response.data);
-    characters.value = response.data.results;
-    info.value = response.data.info;
+async function getCharacters() {
+    try {
+        let response = await axios.get('https://rickandmortyapi.com/api/character', {
+            params: {
+                page:  currentPage.value,
+                name: searchValue.value
+            }
+        });
+        console.log(response.data);
+        characters.value = response.data.results;
+        info.value = response.data.info;
+    } catch(err) {
+        console.log(err);
+        error.value = 'No results found';
+        characters.value = [];
+        info.value = null;
+    }
 }
 
-async function next(){
+async function next() {
     currentPage.value++;
-    await getCharacters(info.value.next);
+    await getCharacters();
 }
 
-async function prev(){
+async function prev() {
     currentPage.value--;
-    await getCharacters(info.value.prev);
+    await getCharacters();
+}
+
+async function page(page) {
+    currentPage.value = page;
+    await getCharacters();
+}
+
+async function search() {
+    error.value = '';
+    currentPage.value = '';
+    await getCharacters();
 }
 
 </script>
 <template>
-    <Pagination :info="info" :current="currentPage" @next="next" @prev="prev"></Pagination>
+    <div class="field has-addons">
+        <div class="control is-expanded">
+            <input v-model="searchValue" class="input" type="text" placeholder="Find character">
+        </div>
+        <div class="control">
+            <button @click="search" class="button is-info">
+                Search
+            </button>
+        </div>
+    </div>
+
+    <Pagination v-if="info" :info="info" :current="currentPage" @next="next" @prev="prev" @page="page"></Pagination>
     <div class="columns is-multiline">
         <div v-for="character in characters" class="column is-one-quarter">
             <CharacterCard :character="character"></CharacterCard>
         </div>
     </div>
+    <h1 v-if="error" class="is-size-1">{{ error }}</h1>
 </template>
