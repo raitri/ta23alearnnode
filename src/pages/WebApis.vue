2b7b586e51c42f63cc0fd1b18bc5fdc47db8689f
Rait Rips <rait.rips@tptlive.ee>
2025-04-08 11:12:22 +0300

    Add WebApis

---
+++ src/pages/WebApis.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+function vibrate() {
+    navigator.vibrate(200);
+}
+
+function SOS() {
+    navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]);
+}
+
+function location() {
+    navigator.geolocation.getCurrentPosition((event) => {
+        console.log(event);
+    }, (error) => {
+        console.log('error', error);
+    });
+}
+
+</script>
+
+<template>
+    <button @click="vibrate" class="button is-primary">Vibrate</button>
+    <button @click="SOS" class="button is-primary">SOS</button>
+    <button @click="location" class="button is-warning">Locate</button>
+</template>
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ export default {
     compress: true,
     port: 9000,
     historyApiFallback: true,
+    allowedHosts: 'all',
   },
   module: {
     rules: [
