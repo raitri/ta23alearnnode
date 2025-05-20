1f06a825eafd6572c6e9e635e939f45209f8d1b8
Rait Rips <rait.rips@tptlive.ee>
2025-05-20 11:39:21 +0300

    Add wumo scraper

---
+++ bin/WUMOscraper.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import fs from "fs";
+
+function cacheGet(name){
+    if(fs.existsSync(`./cache/${name}.html`)) {
+        return fs.readFileSync(`./cache/${name}.html`);
+    }
+    return false;
+}
+
+function cacheSet(name, value){
+    if(!fs.existsSync('./cache')) {
+        fs.mkdirSync('./cache');
+    }
+    fs.writeFileSync(`./cache/${name}.html`, value);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let baseUrl = 'http://wumo.com';
+let url = baseUrl + '';
+for(let i = 0; i < 10; i++) {
+    let name = url.replaceAll('/', '').replaceAll(':', '');
+    let data = cacheGet(name);
+    if(!data) {
+        console.log('LIVE REQUEST!!!!!!!!!!')
+        await sleep(1000);
+        let res = await axios.get(url);
+        data = res.data;
+        cacheSet(name, data);
+    }
+    //console.log(res.data);
+    const $ = cheerio.load(data);
+    let src = baseUrl + $('artcile.wumo img').attr('src');
+    let title = $('artcile.wumo img').attr('alt');
+    console.log(src);
+    console.log(title);
+    url = baseUrl + $('artcile.wumo img').attr('href');
+    console.log(url);
+}
